ddce611a3c32b8a86f66e2e2b530c8ee30c06f37
b9aurav <a9aurav@gmail.com>
2025-06-11 11:30:54 +0530

    Update package dependencies and enhance order service methods for better order management

--- dist/orders/orders.service.d.ts
+++ dist/orders/orders.service.d.ts
@@ -30,4 +30,40 @@ export declare class OrdersService {
             }[];
         };
     }>;
+    getAllOrders(userId?: string, status?: string): Promise<{
+        orders: {
+            id: string;
+            date: Date;
+            total: number;
+            status: OrderStatus;
+            user: {
+                id: string;
+                email: string;
+                name: string;
+            };
+            items: {
+                id: string;
+                quantity: number;
+                price: number;
+                product: {
+                    id: string;
+                    name: string;
+                    images: string[];
+                };
+            }[];
+            address: import("../users/entities/address.entity").Address;
+            tracking_number: string;
+            payment_method: string;
+            transaction_id: string;
+        }[];
+    }>;
+    updateOrderStatus(orderId: string, updateOrderStatusDto: {
+        status: OrderStatus;
+    }): Promise<{
+        message: string;
+        order: {
+            id: string;
+            status: OrderStatus;
+        };
+    }>;
 }
